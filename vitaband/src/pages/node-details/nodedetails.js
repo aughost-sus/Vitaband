fb08c6979f6687e8468b775ec919e0a0253aaaaf
rigel <rekkles0123@gmail.com>
2022-04-06 13:20:48 +0800

    ....

--- vitaband/src/pages/node-details/nodedetails.js
+++ vitaband/src/pages/node-details/nodedetails.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Navbar from '../Navbar/navbar'
-
+import "../node-details/nodedetails.css"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import {MdKeyboardArrowLeft } from 'react-icons/md'
 
 const Nodedetails= () => {
     return (
         <div className='hahaha'>
             <Navbar />
             { /* start coding here */ }
-            <div className='homepage'> hahasdasdaha</div>
+            <MdKeyboardArrowLeft className='icon'/><p>Back to Home</p>
+            <div className='nodedetails'> 
+            <div className="status">
+                <p>STATUS</p>
+                <div className="indicator" />
+                <span>ACTIVE NODE</span>
+                <div className="nodename">
+                    <p>NODE</p>
+                    <span>03</span>
+                </div>
+            </div>
+
+             </div>
+
+           
         </div>
     );
  }
